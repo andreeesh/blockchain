7a90ed204c89eb4b8def5908a4d192fa71186d47
Andres Hernandez <andres@moka.tv>
2022-06-02 14:31:18 -0300

    Progress

--- blockchain/index.js
+++ blockchain/index.js
@@ -1,5 +1,8 @@
 const Block = require('./block');
 const {cryptoHash } = require('../util');
+const { REWARD_INPUT, MINING_REWARD } = require('../config');
+const Transaction = require('../wallet/transaction');
+const Wallet = require('../wallet');
 
 class Blockchain {
 	constructor() {
@@ -60,6 +63,46 @@ class Blockchain {
 		
 		console.log('Replacing chain with', chain);
 		this.chain = chain;
+	}
+
+	validTransactionData({ chain }) { 
+		for (let i = 1; i < chain.length; i++) {
+			const block = chain[i];
+			let rewardTransactionCount = 0;
+
+			for (let transaction of block.data) { 
+				if (transaction.input.address === REWARD_INPUT.address) {
+					rewardTransactionCount += 1;
+
+					if (rewardTransactionCount > 1) {
+						console.error('Miner rewards exceed limit');
+						return false;
+					}
+
+					if (Object.values(transaction.outputMap)[0] !== MINING_REWARD) {
+						console.error('Miner reward amount is invalid');
+						return false;
+					}
+				} else {
+					if (!Transaction.validTransaction(transaction)) {
+						console.error('Invalid transaction');
+						return false;
+					}
+
+					const trueBalance = Wallet.calculateBalance({
+						chain: this.chain,
+						address: transaction.input.address
+					});
+
+					if (transaction.input.amount != trueBalance) {
+						console.error('Invalid input amount');
+						return false;
+					}
+				}
+			}
+		}
+
+		return true;
 	}
 }
 
